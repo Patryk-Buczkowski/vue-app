84adc8ac464e493102a54e698a9a277faab646b3
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2024-09-13 17:48:49 +0200

    loop v-for, v-bind:class

--- src/App.vue
+++ src/App.vue
@@ -1,98 +1,59 @@
+<script>
+import tasks from './todos';
+console.log({ tasks })
+
+export default {
+  data() {
+    return {
+      tasks
+    }
+  }
+}
+</script>
+
 <template>
   <div id="root">
     <div class="todoapp">
       <h1 class="todoapp__title">todos</h1>
       <div class="todoapp__content">
         <header class="todoapp__header">
-          <button
-            type="button"
-            class="todoapp__toggle-all"
-            data-cy="ToggleAllButton"
-          ></button>
+          <button type="button" class="todoapp__toggle-all" data-cy="ToggleAllButton"></button>
+
           <form>
-            <input
-              data-cy="NewTodoField"
-              type="text"
-              class="todoapp__new-todo"
-              placeholder="What needs to be done?"
-              value=""
-            />
+            <input data-cy="NewTodoField" type="text" class="todoapp__new-todo" placeholder="What needs to be done?"
+              value="" />
           </form>
         </header>
+
         <section class="todoapp__main" data-cy="TodoList">
-          <div data-cy="Todo" class="todo completed">
-            <label class="todo__status-label"
-              ><input
-                data-cy="TodoStatus"
-                type="checkbox"
-                class="todo__status" /></label
-            ><span data-cy="TodoTitle" class="todo__title">dupa</span
-            ><button type="button" class="todo__remove" data-cy="TodoDelete">
-              ×
-            </button>
-            <div data-cy="TodoLoader" class="modal overlay">
-              <div class="modal-background has-background-white-ter"></div>
-              <div class="loader"></div>
-            </div>
-          </div>
-          <div data-cy="Todo" class="todo completed">
-            <label class="todo__status-label"
-              ><input
-                data-cy="TodoStatus"
-                type="checkbox"
-                class="todo__status" /></label
-            ><span data-cy="TodoTitle" class="todo__title">kupa</span
-            ><button type="button" class="todo__remove" data-cy="TodoDelete">
-              ×
-            </button>
-            <div data-cy="TodoLoader" class="modal overlay">
-              <div class="modal-background has-background-white-ter"></div>
-              <div class="loader"></div>
-            </div>
-          </div>
-          <div data-cy="Todo" class="todo">
-            <label class="todo__status-label"
-              ><input
-                data-cy="TodoStatus"
-                type="checkbox"
-                class="todo__status" /></label
-            ><span data-cy="TodoTitle" class="todo__title">kita</span
-            ><button type="button" class="todo__remove" data-cy="TodoDelete">
-              ×
-            </button>
+          <div v-for="task of tasks" data-cy="Todo" class="todo" :class="{ completed: task.completed }">
+            <label class="todo__status-label">
+              <input data-cy="TodoStatus" type="checkbox" class="todo__status" /></label>
+            <span data-cy="TodoTitle" class="todo__title">{{ task.title }}</span>
+
+            <button type="button" class="todo__remove" data-cy="TodoDelete">×</button>
+
             <div data-cy="TodoLoader" class="modal overlay">
               <div class="modal-background has-background-white-ter"></div>
               <div class="loader"></div>
             </div>
           </div>
         </section>
+
         <footer class="todoapp__footer" data-cy="Footer">
           <span class="todo-count" data-cy="TodosCounter">1 items left</span>
           <nav class="filter" data-cy="Filter">
-            <a href="#/" class="filter__link selected" data-cy="FilterLinkAll"
-              >All</a
-            ><a href="#/Active" class="filter__link" data-cy="FilterLinkActive"
-              >Active</a
-            ><a
-              href="#/Completed"
-              class="filter__link"
-              data-cy="FilterLinkCompleted"
-              >Completed</a
-            >
+            <a href="#/" class="filter__link selected" data-cy="FilterLinkAll">All</a>
+            <a href="#/Active" class="filter__link" data-cy="FilterLinkActive">Active</a>
+            <a href="#/Completed" class="filter__link" data-cy="FilterLinkCompleted">Completed</a>
           </nav>
-          <button
-            type="button"
-            class="todoapp__clear-completed"
-            data-cy="ClearCompletedButton"
-          >
+
+          <button type="button" class="todoapp__clear-completed" data-cy="ClearCompletedButton">
             Clear completed
           </button>
         </footer>
       </div>
-      <div
-        data-cy="ErrorNotification"
-        class="notification is-danger is-light has-text-weight-normal hidden"
-      >
+      <div data-cy="ErrorNotification" class="notification is-danger is-light has-text-weight-normal hidden">
         <button data-cy="HideErrorButton" type="button" class="delete"></button>
       </div>
     </div>
